d47aebc895299abb02f3bbab2e67c81734480d1b
Xuan-05 <1810465650@qq.com>
2026-04-05 23:29:58 +0800

    后端测试类

---
+++ backend/awards-backend/src/test/java/com/university/awards/AwardsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.awards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwardsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
